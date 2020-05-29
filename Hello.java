d0602e5f46ece5ea920d208c309a7c49ad1913db
wangsen <wangsen@newcapec.com>
2020-05-29 13:51:00 +0800

    第四次提交....

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello
 	public static void main(String[] args){
 		System.out.println("hello world...");
 		System.out.println("11111111111111");
+		System.out.println("2222222222222");
 	}
 }
