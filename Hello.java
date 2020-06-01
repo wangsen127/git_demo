fb3eba759813000be28db5f995c605312cacf3bf
wangsen <wangsen@newcapec.com>
2020-06-01 09:25:30 +0800

    第七次提交，演示GUI版

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@ public class Hello
 {
 	public static void main(String[] args){
 		System.out.println("hello world...");
-		System.out.println("11111111111111");
-		System.out.println("2222222222222");
+		System.out.println("2222222222222ÐÞ¸Ä");
+		for(int i=0;i<=10;i++){
+			System.out.println("´òÓ¡£º" + i);
+		}
 	}
 }
